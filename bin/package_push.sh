76f8eb93bc28e989a005076c5082fc11fee9d5ca
Thomas Riboulet <riboulet@gmail.com>
2018-02-18 11:34:49 +0100

    remove gem at the end to avoid travis errors

--- bin/package_push.sh
+++ bin/package_push.sh
@@ -10,6 +10,8 @@ for file in `ls *gem`; do
     curl -s -o /dev/null -w "%{http_code}" -F package=@$file "https://${FURY_TOKEN}@push.fury.io/finalcad/" | grep "200"
     if [ $? -ne 0 ]; then
         echo "Could not upload package"
+        rm *.gem
         exit 1
     fi
+    rm *.gem
 done
